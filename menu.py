3e9a009f6e55374da7ad0af0e0b7bec59e528000
Sp0ge <narn01style@gmail.com>
2024-04-22 15:44:40 +0300

    dev

--- menu.py
+++ menu.py
@@ -1,13 +1,13 @@
-from rich.console import Console as RichC
-
-RichC = RichC()
+from consolemenu import SelectionMenu
+from consolemenu.items import *
 
 class Main():
     def __init__(self):
-        self.menu  = consolemenu.ConsoleMenu(title="My Game", subtitle="In development", clear_screen=True)
-        self.gen()
-        
-    def gen(self):
-        
+        self.title = "Main menu"
+        self.options = ["Start","Load"]
+        self.menu = SelectionMenu(self.options,self.title,show_exit_option=True)
+
     def show(self):
         self.menu.show()
+        self.menu.join()
+        return self.menu.selected_option
